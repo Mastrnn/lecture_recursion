0b5d153449e841a9534eeda49b700b9f21903478
Marek Strnad <mastrn@seznam.cz>
2024-04-22 12:27:15 +0200

    1

--- fibo.py
+++ fibo.py
@@ -1,6 +1,16 @@
+def recursive_nth_fibo(n):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return recursive_nth_fibo(n-2) + recursive_nth_fibo(n-1)
+
 
 def main():
-    pass
+    n=4
+    fib=[recursive_nth_fibo(num)for num in range(1,n+1)]
+    print(fib)
 
 
 if __name__ == "__main__":
